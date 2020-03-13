10e684fd35f6d5c7e5bbfa39da0690ab04946190
CarlosAriel98 <c.amarquez14@hotmail.com>
2020-03-13 16:52:59 -0600

    Se agregar los objetos que contrendran los datos del programa

--- autos.py
+++ autos.py
@@ -4,5 +4,19 @@
 #          moto en el estacionamiento
 #Autor: Carlos Ariel Molina Marquez
 #Fecha 13/marzo/2020
+import datetime
 
-class 
+
+class autos:
+    """
+    Controla el ingreso de los vehiculos al estacionamiento
+    y guarda la informacion
+    """
+
+    def __init__(self, placa="", marca="", modelo="", tipo_vehiculo="", hora_ingreso="", estado=""):
+        self.placa = placa
+        self.marca = marca
+        self.modelo = modelo
+        self.tipo_vehiculo = tipo_vehiculo
+        self.hora_ingreso = datetime.time.today()
+        self.estado = True
