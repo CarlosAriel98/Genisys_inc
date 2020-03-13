ea94407f82df9b0e8a84dacfe59891d02cbb683e
CarlosAriel98 <c.amarquez14@hotmail.com>
2020-03-13 17:41:21 -0600

    Se agregaron las fuciones de encontra un auto estacionado y el ticket de salida

--- servicio_parqueo.py
+++ servicio_parqueo.py
@@ -10,7 +10,49 @@ class servicio_estacionamiento:
 
     def __init__(self):
         """Inicia todos los procesos"""
-        my_parking = list()
+        mi_parqueo = list()
 
     
+
+    def agregar_auto(self, placa = "", marca = "", modelo = "", tipo_vehiculo= "", cobro= float):
+        """ Agrega los vehiculos a la lista"""
+        self.mi_parqueo.append(autos(placa, marca, modelo, tipo_vehiculo,cobro))
+
+
+    def autos_estacionados(self, pago, placa):
+        """ 
+        Encuentra el auto en una lista para asi poder ver el tiempo que a estada 
+        para poder realisar el cobro
+        """
+        auto = self._buscar_autos(placa)
+
+        if auto:
+            auto.placa=placa
+            auto.marca=marca
+            auto.modelo=modelo
+            auto.tipo_vehiculo = tipo_vehiculo
+            auto.cobro=cobro
+            return True
+        else:
+            print("No existe un vehiculo con la placa {0} en el estacionamiento ".format(placa))
+        
+
+    def _buscar_autos(self, placa):
+        """
+        funcion privada que se encarga de buscar un vehiculos en el estacionamiento
+        """
+        for auto in self.mi_parqueo:
+            if str(auto.placa) == str(placa):
+                return auto
+        
+        return None
+
+
+    def ticket_salida(self):
+        """ Toma nota de la hora de salida del cliente y poder realizar su cobro """
+        total = 0.0
+        for x in self.mi_parqueo:
+            total=x.cobro=total
+
+        print("El costo total es de :",total)
 
