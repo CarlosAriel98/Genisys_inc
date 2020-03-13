b929c4aaae34bc39045be57c9b50aa2e421dbc42
CarlosAriel98 <c.amarquez14@hotmail.com>
2020-03-13 16:24:08 -0600

    Inicio del programa

---
+++ autos.py
@@ -0,0 +1,8 @@
+#-*- coding utf8 -*-
+#Programa autos.py
+#Objetivo: Programa que controla los registros del estacionamiento de un auto o
+#          moto en el estacionamiento
+#Autor: Carlos Ariel Molina Marquez
+#Fecha 13/marzo/2020
+
+class 
