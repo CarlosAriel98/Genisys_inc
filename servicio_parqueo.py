78f324622236ac2e54e3e9f7f35626d87cd69bb6
CarlosAriel98 <c.amarquez14@hotmail.com>
2020-03-13 16:53:53 -0600

    inicio de la clase que controlara el tiempo que un vehiculo esta estacionado

---
+++ servicio_parqueo.py
@@ -0,0 +1,16 @@
+#-*- coding utf8 -*-
+
+from autos import autos
+from datetime import datetime, timedelta
+
+
+class servicio_estacionamiento:
+    """ clase que obtiene el tipo de vehiculo y calcual cuanto tiempo estubo en el estacioamiento"""
+
+
+    def __init__(self):
+        """Inicia todos los procesos"""
+        my_parking = list()
+
+    
+
