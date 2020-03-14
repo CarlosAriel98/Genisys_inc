41dcec4386b7d429b35e42ce7c75b661bf76c299
CarlosAriel98 <c.amarquez14@hotmail.com>
2020-03-13 18:14:27 -0600

    Cambios pequeños

--- autos.py
+++ autos.py
@@ -5,6 +5,7 @@
 #Autor: Carlos Ariel Molina Marquez
 #Fecha 13/marzo/2020
 import datetime
+from datetime import datetime, timedelta
 
 
 class autos:
@@ -13,10 +14,12 @@ class autos:
     y guarda la informacion
     """
 
-    def __init__(self, placa="", marca="", modelo="", tipo_vehiculo="", hora_ingreso="", estado=""):
+    def __init__(self, placa="", marca="", modelo="", tipo_vehiculo="", hora_ingreso="", estado="", cobro=""):
         self.placa = placa
         self.marca = marca
         self.modelo = modelo
         self.tipo_vehiculo = tipo_vehiculo
         self.hora_ingreso = datetime.time.today()
         self.estado = True
+        self.plus_five_hours = datetime.now() + timedelta(hours=5)
+        self.cobro = cobro
